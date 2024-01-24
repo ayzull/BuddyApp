62d3951baec494375744b135f0d1bd33c093625b
syauu <boiling-00arrow@icloud.com>
2024-01-25 01:57:36 +0800

    logout

--- app/src/main/java/com/example/buddyapps2/MainActivity.java
+++ app/src/main/java/com/example/buddyapps2/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.buddyapps2;
 //Hazim
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -53,31 +55,52 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new FriendsFragment()).commit();
             Toast.makeText(this, "My Friends", Toast.LENGTH_SHORT).show();
-        }
-        else if (itemID == R.id.nav_birthday) {
+        } else if (itemID == R.id.nav_birthday) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new BirthdayFragment()).commit();
             Toast.makeText(this, "Birthday", Toast.LENGTH_SHORT).show();
-        }
-        else if (itemID == R.id.nav_chart) {
+        } else if (itemID == R.id.nav_chart) {
             Intent intent = new Intent(MainActivity.this, ReportListActivity.class);
             startActivity(intent);
             Toast.makeText(this, "Report", Toast.LENGTH_SHORT).show();
+        } else if (itemID == R.id.logout) {
+            logoutMenu(MainActivity.this);
         }
 
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            return true;
         }
+
+        public void logoutMenu (MainActivity mainActivity){
+            AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+            builder.setTitle("Logout");
+            builder.setMessage("Are you sure you want to logout?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int i) {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.show();
+        }
+
+        @Override
+        public void onBackPressed () {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+
     }
-
-
-
-}
